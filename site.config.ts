f3bf9c01622fb54f3d256640ec52f7d15f987412
xhans <hanwinhtun334@gmail.com>
2022-10-07 15:51:57 -0700

    fix

--- site.config.ts
+++ site.config.ts
@@ -11,7 +11,7 @@ export default siteConfig({
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'HWT',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
