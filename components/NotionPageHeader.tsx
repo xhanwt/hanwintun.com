ae1b46031e91bc9de5e0c3c73e4c488159c7b440
xhans <hanwinhtun334@gmail.com>
2022-10-26 18:06:38 -0700

    fix

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -5,6 +5,7 @@ import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
 import { Header, Breadcrumbs, Search, useNotionContext } from 'react-notion-x'
 import * as types from 'notion-types'
 
+
 import { DarkModeSwitch } from 'react-toggle-dark-mode';
 import { useDarkMode } from 'lib/use-dark-mode'
 import { navigationStyle, navigationLinks, isSearchEnabled } from 'lib/config'
