c2f5ebecea91db9a6c5b2c375f576d121f251bfc
xhans <hanwinhtun334@gmail.com>
2022-10-30 12:21:46 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   staticPageGenerationTimeout: 300,
+  optimizeFonts: false,
   images: {
     domains: [
       'www.notion.so',
