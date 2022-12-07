d41edbe2a38bc8d8e566fe3af368d1dc8d3ac09e
xhans <hanwinhtun334@gmail.com>
2022-12-06 21:33:38 -0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -34,7 +34,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2023 {config.author}</div>
+      <div className={styles.copyright}>© 2022, {config.author}.</div>
 
       <div className={styles.settings}>
         < StatusBar />
